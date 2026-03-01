7370aff8f6804ec831b380a23441f0185248674d
HeLeha282 <aleksejnadmitov696@gmail.com>
2026-03-01 19:17:12 +0300

    Completed Task4

--- src/main/java/tasks/Task4.java
+++ src/main/java/tasks/Task4.java
@@ -22,6 +22,11 @@ public class Task4 {
   }
 
   public List<ApiPersonDto> convert(List<Person> persons) {
-    return new ArrayList<>();
+    List<ApiPersonDto> convertedPersons = persons
+        .stream()
+        .map(personConverter::convert)
+        .toList();
+
+    return convertedPersons;
   }
 }
