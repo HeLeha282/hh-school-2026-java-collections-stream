71c90039d37491267c7399427113db6d1ad5facd
HeLeha282 <aleksejnadmitov696@gmail.com>
2026-03-01 20:31:38 +0300

    Completed Task6

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -2,10 +2,9 @@ package tasks;
 
 import common.Area;
 import common.Person;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /*
 Имеются
@@ -19,6 +18,26 @@ public class Task6 {
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+    // Создаю словарь (id региона - Название региона)
+    Map<Integer, String> areasNameById = areas
+        .stream()
+        .collect(Collectors.toMap(
+            Area::getId,
+            Area::getName));
+
+
+    Set<String> personDescriptions = persons
+        .stream()
+        .flatMap(person -> {
+          Set<Integer> areaIds = personAreaIds.get(person.id()); // Получаю регионы персоны
+          String personName = person.firstName(); // Получаю имя персоны
+
+          // Для каждого региона персоны создаю строчку типа "Имя - Регион", и в итоге возвращаю Stream из таких строк
+          return areaIds.stream()
+              .map(areaId -> personName + " - " + areasNameById.get(areaId));
+        })
+        .collect(Collectors.toSet()); // Собираю все строки в Set
+
+    return personDescriptions;
   }
 }
