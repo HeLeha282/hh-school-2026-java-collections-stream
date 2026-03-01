0e75778cf096c19d7535af2eb330625de7a71a15
HeLeha282 <aleksejnadmitov696@gmail.com>
2026-03-01 18:23:36 +0300

    Completed Task1

--- src/main/java/tasks/Task1.java
+++ src/main/java/tasks/Task1.java
@@ -2,9 +2,10 @@ package tasks;
 
 import common.Person;
 import common.PersonService;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /*
 Задача 1
@@ -21,8 +22,31 @@ public class Task1 {
     this.personService = personService;
   }
 
+  //Итоговая сложность алгоритма сортировки коллекции, которая приходит из сервиса - O(M+N)
+  //При решении задачи предполагаю, что в personIds и persons не может быть элементов со значением null
   public List<Person> findOrderedPersons(List<Integer> personIds) {
-    Set<Person> persons = personService.findPersons(personIds);
-    return Collections.emptyList();
+    // Пусть N - количество элементов в persinIds
+    Set<Person> persons = personService.findPersons(personIds);//возвращает Set из M элементов; где M<=N
+
+    // Создаем словарь, где ключом является id, а значением объект класса Person - сложность O(M)
+    Map<Integer, Person> mapPersons =  persons.stream()
+        .collect(Collectors.toMap(
+            Person::id,
+            Function.identity()
+        ));
+
+    // Создаем список, чтобы заполнить его отсортированными объектами класса Person - сложность O(1)
+    List<Person> sortedPersons = new ArrayList<>();
+
+    // Прохожусь по всем id в листе, который нам изначально передали, для каждого получаю
+    // значение из словаря по такому id и добавляю это значение в лист sortedPersons - сложность O(N)
+    personIds.forEach(
+        personId -> {
+          Person person = mapPersons.get(personId);
+          sortedPersons.add(person);
+        });
+
+    // Возвращаю отсортированный List
+    return sortedPersons;
   }
 }
