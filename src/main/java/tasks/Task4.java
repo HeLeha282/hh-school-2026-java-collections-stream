61c2ad30487ec43d1bda97476976ba7388410592
HeLeha282 <aleksejnadmitov696@gmail.com>
2026-03-02 00:08:09 +0300

    refactor: rewrite Task4 with better structure

--- src/main/java/tasks/Task4.java
+++ src/main/java/tasks/Task4.java
@@ -22,11 +22,10 @@ public class Task4 {
   }
 
   public List<ApiPersonDto> convert(List<Person> persons) {
-    List<ApiPersonDto> convertedPersons = persons
+
+    return persons
         .stream()
         .map(personConverter::convert)
         .toList();
-
-    return convertedPersons;
   }
 }
