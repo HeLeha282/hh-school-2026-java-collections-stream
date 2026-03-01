054bf596bc9942c4994ff4f3aca4d202900b61b4
HeLeha282 <aleksejnadmitov696@gmail.com>
2026-03-02 00:07:49 +0300

    refactor: rewrite Task2 with better structure

--- src/main/java/tasks/Task2.java
+++ src/main/java/tasks/Task2.java
@@ -19,13 +19,10 @@ public class Task2 {
   public static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
                                                      Collection<Person> persons2,
                                                      int limit) {
-    List<Person> limitPersonList = Stream
+    return Stream
         .concat(persons1.stream(), persons2.stream())
         .sorted(Comparator.comparing(Person::createdAt))
-        //.sorted((person1, person2) -> person1.createdAt().compareTo(person2.createdAt()) изначально хотел сделать так
         .limit(limit)
         .toList();
-
-    return limitPersonList;
   }
 }
