10a7d623db70b417dc7f85698f78a07fce493fe1
HeLeha282 <aleksejnadmitov696@gmail.com>
2026-03-01 18:43:56 +0300

    Completed Task2

--- src/main/java/tasks/Task2.java
+++ src/main/java/tasks/Task2.java
@@ -3,7 +3,10 @@ package tasks;
 import common.Person;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
 Задача 2
@@ -16,6 +19,13 @@ public class Task2 {
   public static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
                                                      Collection<Person> persons2,
                                                      int limit) {
-    return new ArrayList<>();
+    List<Person> limitPersonList = Stream
+        .concat(persons1.stream(), persons2.stream())
+        .sorted(Comparator.comparing(Person::createdAt))
+        //.sorted((person1, person2) -> person1.createdAt().compareTo(person2.createdAt()) изначально хотел сделать так
+        .limit(limit)
+        .toList();
+
+    return limitPersonList;
   }
 }
